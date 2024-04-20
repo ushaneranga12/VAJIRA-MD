c095ca11d8d4d14582958de7b28bb8062552abf4
ushaneranga12 <165865546+ushaneranga12@users.noreply.github.com>
2024-04-20 07:38:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,12 +22,12 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = 'ᴠᴀᴊɪʀᴀ ʀᴀᴛʜɴᴀʏᴀᴋᴀ';
-global.owner = ['94719199757','94703475691'];
+global.ownername = 'ᴜꜱʜᴀɴ ᴇʀᴀɴɢᴀ';
+global.owner = ['94723984917','94762205705'];
 global.premium = ['94719199757','94703475691'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
-global.caption = ' 👨‍💻 ＶＡＪＩＲＡ ＭＤ V3 👨‍💻';
+global.author = ' 𝙐𝙎𝙃𝘼𝙉 𝙈𝘿';
+global.caption = ' 👨‍💻 ＵＳＨＡＮ ＭＤ V3 👨‍💻';
 global.sessionName = 'session';
 global.prefa = ['!', '.'];
 global.sp = '✪';
